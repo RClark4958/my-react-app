ef6c9e6d0e124bda73def81969d34f7627442a86
RCLARK4958 <RICKDCLARK48@GMAIL.COM>
2019-02-21 16:18:12 -0700

    to_do

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,67 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { Component } from 'react'
+import "./App.css"
+import TodoList from "./TodoList"
 
-const Food = () => <h2>Food</h2>;
-const Drinks = ({ routes }) => (
+class App extends Component {
+    render () {
+        return (
+            <div className="App">
+                <TodoList />
+            </div>
+        )
+    }
+}
 
-    <div>
-
-        <h2>Drinks</h2>
-            <ul>
-                <li>
-                    <Link to="/drinks/soda">Soda</Link>
-                </li>
-                <li>
-                    <Link to="/drinks/juice">Juice</Link>
-                </li>
-            </ul>
-
-            {routes.map((route, i) => <RouteWithSubRoutes key ={i} {...route} />)}
-
-    </div>
-);
-
-const Soda = () => <h3>Soda</h3>;
-const Juice = () => <h3>Juice</h3>;
-
-const routes = [
-    {
-        path: "/drinks",
-        component: Drinks,
-        routes: [
-            {
-                path:"/drinks/soda",
-                component: Soda
-            },
-            {
-                path: "/drinks/juice",
-                component: Juice
-            }
-        ]
-    }  
-];
-
-const RouteWithSubRoutes = route => (
-    <Route
-        path={route.path}
-        render={props => (
-            <route.component {...props} routes={route.routes} />
-        )}
-    />
-);
-
-const RickRick = () => (
-    <Router>
-        <div>
-            <ul>
-                <li>
-                    <Link to="/Drinks">Drinks</Link>
-                </li>
-            </ul>
-
-            {routes.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)}
-        </div>
-    </Router>
-);
-
-export default RickRick;
+export default App
